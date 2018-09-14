50f0eb26c2c514ec7078125f1ca2092cf040060c
Remco <remco.brilstra@gmail.com>
2018-09-14 13:49:16 +0200

    Add project files.

---
+++ JustCodeStuff.DotNetCore.Sample/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace JustCodeStuff.DotNetCore.Sample.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
